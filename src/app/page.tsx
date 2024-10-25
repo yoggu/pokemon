7fac41f93bb2da105e2ce2e0148cb33f6beca196
Yannick Bättig <yannick.baettig@gmail.com>
2024-10-25 22:08:38 +0200

    fix packages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
 import PokemonList from "./PokemonList";
 import Search from "./Search";
 
-export default function PokemonListPage({ searchParams }: { searchParams: any }) {
+export default function PokemonListPage({
+  searchParams,
+}: {
+  searchParams: any;
+}) {
   return (
     <>
       <h1 className="font-semibold text-4xl mb-6">Pokemon Index</h1>
       <Search />
-      <PokemonList limit={searchParams.limit} offset={searchParams.offset} name={searchParams.name} />
+      <PokemonList
+        limit={searchParams.limit}
+        offset={searchParams.offset}
+        name={searchParams.name}
+      />
     </>
   );
 }
